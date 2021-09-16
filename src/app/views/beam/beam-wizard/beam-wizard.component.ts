bf49abf85c5ba19641fc62a7b414bf12f9492dda
kideveloper612 <happydev0320@gmail.com>
2021-09-16 04:45:32 -0700

    Complete multiple form except removal of validation

--- src/app/views/beam/beam-wizard/beam-wizard.component.ts
+++ src/app/views/beam/beam-wizard/beam-wizard.component.ts
@@ -18,10 +18,14 @@ export class BeamWizardComponent implements OnInit {
 
   firstFormSubOneGroup: FormGroup;
   firstFormSubTwoGroup: FormGroup;
+  secondFormSubOneGroup: FormGroup;
+  secondFormSubTwoGroup: FormGroup;
+  thirdFormSubGroup: FormGroup;
 
   floorFlag: Boolean = false;
   roofFlag: Boolean = false;
   wallFlag: Boolean = false;
+  onlyDesign: Boolean = false;
 
   @ViewChild('stepper') private myStepper: MatStepper;
 
@@ -31,19 +35,30 @@ export class BeamWizardComponent implements OnInit {
   ngOnInit() {
     this.firstFormGroup = this.fb.group({});
     this.firstFormSubOneGroup = this.fb.group({
-      firstSubOneCtrl: ['', Validators.required]
+      firstSubOneCtrl: []
     });
     this.firstFormSubTwoGroup = this.fb.group({
-      firstSubSideOneCtrl: ['', Validators.required],
-      firstSubSideTwoCtrl: ['', Validators.required]
+      firstSubSideOneCtrl: [],
+      firstSubSideTwoCtrl: []
     });
     this.secondFormGroup = this.fb.group({});
+    this.secondFormSubOneGroup = this.fb.group({
+      secondSubOneCtrl: []
+    });
+    this.secondFormSubTwoGroup = this.fb.group({
+      secondSubSideOneCtrl: [],
+      secondSubSideTwoCtrl: []
+    });
     this.thirdFormGroup = this.fb.group({});
+    this.thirdFormSubGroup = this.fb.group({
+      thirdSubOneCtrl: [],
+      thirdSubTwoCtrl: []
+    });
     this.forthFormGroup = this.fb.group({
       forthCtrl: ['', Validators.required]
     });
     this.fifthFormGroup = this.fb.group({
-      fifthCtrl: ['', Validators.required]
+      fifthCtrl: []
     });
     this.sixthFormGroup = this.fb.group({
       sixthCtrl: ['', Validators.required]
@@ -65,7 +80,6 @@ export class BeamWizardComponent implements OnInit {
   }
 
   goBack(step: number = 0, index: number = 1) {
-    console.log(step, index, this.floorFlag)
     switch (step) {
       case 0:
         if (this.floorFlag) this.backAction(index);
@@ -82,6 +96,11 @@ export class BeamWizardComponent implements OnInit {
         else this.backAction(1)
         break;
 
+      case 3:
+        if (this.onlyDesign) this.backAction(index);
+        else this.backAction(1)
+        break;
+
       default:
         break;
     }
@@ -90,52 +109,106 @@ export class BeamWizardComponent implements OnInit {
   goForward(step: number = 0, index: number = 1) {
     switch (step) {
       case 0:
-        if (index === 1) this.floorFlag = false;
-        else this.floorFlag = true;
+        if (index === 1) {
+          this.floorFlag = false;
+
+          if (this.firstFormSubTwoGroup.get('firstSubSideOneCtrl').value === null)
+            this.firstFormSubTwoGroup.controls['firstSubSideOneCtrl'].setValidators([Validators.required]);
+
+          this.firstFormSubTwoGroup.controls['firstSubSideOneCtrl'].updateValueAndValidity();
+
+          if (this.firstFormSubTwoGroup.get('firstSubSideTwoCtrl').value === null)
+            this.firstFormSubTwoGroup.controls['firstSubSideTwoCtrl'].setValidators([Validators.required]);
+
+          this.firstFormSubTwoGroup.controls['firstSubSideTwoCtrl'].updateValueAndValidity();
+        }
+        else {
+          this.floorFlag = true;
+
+          this.firstFormSubTwoGroup.get['firstSubSideOneCtrl']?.clearValidators();
+          this.firstFormSubTwoGroup.get['firstSubSideOneCtrl']?.updateValueAndValidity();
+
+          this.firstFormSubTwoGroup.get['firstSubSideTwoCtrl']?.clearValidators();
+          this.firstFormSubTwoGroup.get['firstSubSideTwoCtrl']?.updateValueAndValidity();
+        }
         break;
 
       case 1:
-        if (index === 1) this.roofFlag = false;
-        else this.roofFlag = true;
+        if (index === 1) {
+          this.roofFlag = false;
+
+          if (this.secondFormSubTwoGroup.get('secondSubSideOneCtrl').value === null)
+            this.secondFormSubTwoGroup.controls['secondSubSideOneCtrl'].setValidators([Validators.required]);
+
+          this.secondFormSubTwoGroup.controls['secondSubSideOneCtrl'].updateValueAndValidity();
+
+          if (this.secondFormSubTwoGroup.get('secondSubSideTwoCtrl').value === null)
+            this.secondFormSubTwoGroup.controls['secondSubSideTwoCtrl'].setValidators([Validators.required]);
+
+          this.secondFormSubTwoGroup.controls['secondSubSideTwoCtrl'].updateValueAndValidity();
+        }
+        else {
+          this.roofFlag = true;
+
+          this.firstFormSubTwoGroup.get['secondSubSideOneCtrl']?.clearValidators();
+          this.firstFormSubTwoGroup.get['secondSubSideOneCtrl']?.updateValueAndValidity();
+
+          this.firstFormSubTwoGroup.get['secondSubSideTwoCtrl']?.clearValidators();
+          this.firstFormSubTwoGroup.get['secondSubSideTwoCtrl']?.updateValueAndValidity();
+        }
         break;
 
       case 2:
-        if (index === 1) this.wallFlag = false;
-        else this.wallFlag = true;
+        if (index === 1) {
+          this.wallFlag = false;
+
+          if (this.thirdFormSubGroup.get('thirdSubOneCtrl').value === null)
+            this.thirdFormSubGroup.controls['thirdSubOneCtrl'].setValidators([Validators.required]);
+
+          this.thirdFormSubGroup.controls['thirdSubOneCtrl'].updateValueAndValidity();
+
+          if (this.thirdFormSubGroup.get('thirdSubTwoCtrl').value === null)
+            this.thirdFormSubGroup.controls['thirdSubTwoCtrl'].setValidators([Validators.required]);
+
+          this.thirdFormSubGroup.controls['thirdSubTwoCtrl'].updateValueAndValidity();
+        }
+        else {
+          this.wallFlag = true;
+
+          this.firstFormSubTwoGroup.get['thirdSubOneCtrl']?.clearValidators();
+          this.firstFormSubTwoGroup.get['thirdSubOneCtrl']?.updateValueAndValidity();
+
+          this.firstFormSubTwoGroup.get['thirdSubTwoCtrl']?.clearValidators();
+          this.firstFormSubTwoGroup.get['thirdSubTwoCtrl']?.updateValueAndValidity();
+        }
         break;
+
+      case 3:
+        if (index === 1) {
+          this.onlyDesign = false;
+
+          if (this.fifthFormGroup.get('fifthCtrl').value === null)
+            this.fifthFormGroup.controls['fifthCtrl'].setValidators([Validators.required]);
+
+          this.fifthFormGroup.controls['fifthCtrl'].updateValueAndValidity();
+        }
+        else {
+          this.onlyDesign = true;
+
+          this.firstFormSubTwoGroup.get['fifthCtrl']?.clearValidators();
+          this.firstFormSubTwoGroup.get['fifthCtrl']?.updateValueAndValidity();
+        }
+        break;
+
+      case 4:
+        console.log(step, index);
+        break
 
       default:
         break;
     }
 
     this.forwardAction(index);
-  }
-
-  floorSkip(index: number) {
-    this.floorFlag = true;
-
-    while (index > 0) {
-      this.myStepper.next();
-      index--;
-    }
-  }
-
-  roofSkip(index: number) {
-    this.roofFlag = true;
-
-    while (index > 0) {
-      this.myStepper.next();
-      index--;
-    }
-  }
-
-  wallSkip(index: number) {
-    this.wallFlag = true;
-
-    while (index > 0) {
-      this.myStepper.next();
-      index--;
-    }
   }
 
   submit() {
@@ -145,6 +218,10 @@ export class BeamWizardComponent implements OnInit {
     console.log(this.forthFormGroup.value);
     console.log(this.fifthFormGroup.value);
     console.log(this.sixthFormGroup.value);
+    console.log(this.firstFormSubOneGroup.value);
+    console.log(this.firstFormSubTwoGroup.value);
+    console.log(this.secondFormSubOneGroup.value);
+    console.log(this.secondFormSubTwoGroup.value);
+    console.log(this.thirdFormSubGroup.value);
   }
-
 }
